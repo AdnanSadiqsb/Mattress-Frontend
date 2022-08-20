a258d12e65cf4da7211ec3f461c709452f25f1df
Adnan Sadiq <adnansadiqxyz@gmail.com>
2022-08-20 23:45:03 +0500

    change in design minor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,11 +22,11 @@ export default function Navbar() {
     }
   return (
     <div className={`${stickyNav ? 'sticky' : ''}`}>
-      <img style={{width:'100%', maxHeight:'100px'}} src={logoImg} alt="" />
+      <img style={{width:'100%', maxHeight:'100px', display:`${stickyNav ? 'block' : 'none'}`}}  src={logoImg} alt="" />
 
       <nav className="navbar navbar-expand-lg navbar-dark  " style={{backgroundColor:'#10255D'}}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/" onClick={goHome}>ShkeelFoam</Link>
+    <Link className="navbar-brand nav-heading" to="/" onClick={goHome}>ShakeelFoam</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
